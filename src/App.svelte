f6a1151b7ef1b7321e0a8a3d98d0253ecf3b0bbe
daisykumar <56020145+daisykumar@users.noreply.github.com>
2020-04-08 11:08:44 +0200

    second commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,117 @@
 <script>
-	export let name;
+	
+	let klasse = "faller"
+	
+	let tall1 = 9
+	let tall2 = 5
+	$: fasit = tall1 * tall2
+	let svar = ""
+	let theGameIsOn = true
+	$: riktigsvar = (fasit === svar)
+	$: regnestykke = `${tall1} x ${tall2}`
+	let poeng = 0
+	
+	const lagNyeTall = () => {
+		tall1 =  Math.ceil(Math.random() * 10)
+		tall2 =  Math.ceil(Math.random() * 10)
+	}
+	
+	const sjekkSvar = () => {
+		if(riktigsvar && theGameIsOn) {
+			lagNyeTall()
+			svar = ""	
+			klasse = ""
+			poeng++
+			setTimeout( () => { klasse = "faller" }, 50 )
+		}
+		
+	}
+	
+	const gameOver = () => {
+		theGameIsOn = false
+		console.log("GAME OVER")
+	}
+	
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+
+<section>
+	<header>
+		<div class="points"><label>Your Points</label><p>{poeng}</p></div>
+		<div class="points"><label>Highscore</label><p>0</p></div>
+	</header>	
+	
+	<main>
+		<div on:animationend={gameOver} class="{klasse}">{regnestykke}</div>	
+	</main>
+	
+	<footer>
+		<input type="number" bind:value={svar} on:input={sjekkSvar}>
+	</footer>
+	
+</section>
 
 <style>
-	main {
+	* {
+		margin: 0;
+		box-sizing: border-box;
+	}
+	section{
+		display: grid;
+		grid-template-rows: auto 300px auto;
+		width: 350px;
+		background-color: #eee;
+		margin: 20px auto;
+	}
+	
+	header{
+		background-color: #f9e5bc;
+		padding: 0.5em;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+
+	.points{
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.points p {
+		font-size: 2rem;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+	
+	input {
+		width: 100%;
+		padding: 0.5rem;
+		font-size: 1.75rem;
+		text-align: center;
+		border: none;
+	}
+	
+	input:focus {
+		outline: none;
+	}
+	
+	footer {
+		border: 4px solid lightblue;
+	}
+	
+	@keyframes fallNed {
+		to {
+			transform: translateY(250px);
 		}
 	}
+
+	.faller {
+		animation: fallNed 4s linear forwards;
+	}
+
+	main div {
+		width: 200px;
+		height: 50px;
+		line-height: 50px;
+		margin: 10px auto;
+		background-color: #90EE90;
+		text-align: center;
+		font-size: 2rem;
+	}	
+	
 </style>
